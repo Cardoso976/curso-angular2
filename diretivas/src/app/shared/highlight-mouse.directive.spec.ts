9571860f89cff97a36fadd4e66a14dcd87db6c10
Rodrigo Cardoso <rodrigo.ti@mfrural.com.br>
2018-03-16 08:57:59 -0300

    Aula 34

---
+++ diretivas/src/app/shared/highlight-mouse.directive.spec.ts
@@ -0,0 +1,9 @@
+import { Renderer } from '@angular/core';
+import { HighlightMouseDirective } from './highlight-mouse.directive';
+
+describe('HighlightMouseDirective', () => {
+  it('should create an instance', () => {
+    const directive = Renderer;
+    expect(directive).toBeTruthy();
+  });
+});
