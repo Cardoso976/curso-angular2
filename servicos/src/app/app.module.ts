a88a8ff469f14b48ea1bfc2a8ef6c2072e4c919e
Rodrigo Cardoso <rodrigo.ti@mfrural.com.br>
2018-03-19 11:33:47 -0300

    Aula

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -1,24 +1,23 @@
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
+import { CursosModule } from './cursos/curso.module';
 import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [
-    CursosService
-  ],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
