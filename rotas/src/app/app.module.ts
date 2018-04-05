61eb0afcca7a94fcbf3a85b46b2556acaa7f0fe5
Rodrigo Cardoso <rodrigo.ti@mfrural.com.br>
2018-04-05 09:00:50 -0300

    Aula 53

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { routing } from './app.routing';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { routing } from './app.routing';
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { LoginComponent } from './login/login.component';
 
 const APP_ROUTES: Routes = [
     { path: 'cursos', component: CursosComponent },
+    { path: 'curso/:id', component: CursoDetalheComponent },
     { path: 'login', component: LoginComponent },
     { path: '', component: HomeComponent }
 ];
